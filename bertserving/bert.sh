7ee46df8841f61360f434e4eb637f652276a8f46
swapnil2911 <57068516+swapnil2911@users.noreply.github.com>
2022-02-02 22:42:20 +0530

    model can now be interchanged with ease and the right model can be selected by specifying name in the .env file

--- bertserving/bert.sh
+++ bertserving/bert.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 echo "Starting bert server"
-
-bert-serving-start -cpu -max_batch_size 16 -num_worker 1 -max_seq_len 256 -model_dir /bert/cased_L-12_H-768_A-12
+bert-serving-start -cpu -max_batch_size 16 -num_worker 1 -max_seq_len 256 -model_dir /model
--- bertserving/entrypoint.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-bert-serving-start -cpu -max_batch_size 16 -http_port 8125 -num_worker=$1 -model_dir /model
